5fd17b3e45bfd963bb21009ef1e565164dc20ddb
phassett <phassett@mmc.org>
2019-04-02 17:09:43 +0000

    Summarize gapminder and create plot of Africa life expectancy

---
+++ src/analysis/gapminder/dplyr.R
@@ -0,0 +1,51 @@
+library(here)
+library(tidyverse)
+
+gapminder <- readr::read_csv(here("data/gapminder/raw/gapminder_data.csv"))
+
+mean(gapminder$gdpPercap[gapminder$continent == "Africa"])
+mean(gapminder$gdpPercap[gapminder$continent == "Americas"])
+
+# more efficient method
+year_country_gdp <- select(gapminder, year, country, gdpPercap)
+
+year_country_gdp <- gapminder %>%
+  select(year, country, gdpPercap, continent) %>%
+  filter(continent == "Europe")
+
+
+#alternative filter first then keep
+year_country_gdp <- gapminder %>%
+  filter(continent == "Europe") %>%
+  select(year, country, gdpPercap)
+ 
+year_country_gdp <- gapminder %>%
+  filter(continent == "Africa") %>%
+  select(year, country, lifeExp)
+
+
+# group_by
+ gapminder %>%
+  group_by(continent) %>%
+  summarize(mean_gdpPercap =mean(gdpPercap),
+            sd_gdpPercap = sd(gdpPercap)
+            ) 
+
+ 
+# challenge 2
+ gapminder %>%
+   group_by(country) %>%
+   summarize(mean_lifeExp =mean(lifeExp)) %>%
+   filter(mean_lifeExp == min(mean_lifeExp) | mean_lifeExp == max(mean_lifeExp))
+   
+
+ 
+ 
+ ggplot code:
+   ggplot(data = gapminder, aes(x = year, y = lifeExp, color = continent)) +
+   geom_line() +
+   facet_wrap( ~ country)
+ 
+
+
+
